27d514f244308fc51af97d12e7e4b49bbdbc99d0
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-06-24 12:33:09 -0400

    Add project files.

---
+++ MoodCalendar/MoodCalendar/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MoodCalendar.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MoodCalendar.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
