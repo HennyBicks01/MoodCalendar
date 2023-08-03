496397514034f78b66de987646eaf23bfc55a5c8
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-08-02 22:26:09 -0400

    added commands to global variables

--- MoodCalendarTracker/MoodCalendarTracker/GlobalVariables.cs
+++ MoodCalendarTracker/MoodCalendarTracker/GlobalVariables.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
 
-namespace MoodCalendarTracker.Models
+namespace MoodCalendarTracker
 {
     public static class GlobalVariables
     {
-        public static Dictionary<DateTime, string> DateStatus { get; set; } = new Dictionary <DateTime, string>();
+        public static Dictionary<DateTime, string> DateStatus { get; set; } = new Dictionary<DateTime, string>();
+
+        // Commands
+        public static ICommand GoodCommand => new Command(() => DateStatus[DateTime.Now] = "good");
+        public static ICommand NeutralCommand => new Command(() => DateStatus[DateTime.Now] = "neutral");
+        public static ICommand BadCommand => new Command(() => DateStatus[DateTime.Now] = "bad");
     }
+
 
 }
