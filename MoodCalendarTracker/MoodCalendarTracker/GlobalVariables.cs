8ac640d2e54ce8d8680e8d8e8793d3d05ac858e5
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-08-02 22:23:49 -0400

    created global variable

---
+++ MoodCalendarTracker/MoodCalendarTracker/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoodCalendarTracker.Models
+{
+    public static class GlobalVariables
+    {
+        public static Dictionary<DateTime, string> DateStatus { get; set; } = new Dictionary <DateTime, string>();
+    }
+
+}
